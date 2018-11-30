9433dff0d843d70f071b2931554b0cf60291bdb7
TotoLeCoco <45094027+TotoLeCoco@users.noreply.github.com>
2018-11-30 17:11:35 +0100

    Update SStage.java

--- ProjetConvention/src/app/SStage.java
+++ ProjetConvention/src/app/SStage.java
@@ -12,10 +12,15 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.jms.Destination;
 import javax.jms.JMSException;
+import javax.jms.Message;
 import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
+import javax.jms.ObjectMessage;
 import javax.naming.NamingException;
+import messages.EtatFormulaire;
 import messages.Formulaire;
+import messages.FormulaireEnValidation;
+import messages.ValidOk;
 import org.apache.jasper.tagplugins.jstl.ForEach;
 
 /**
@@ -28,7 +33,7 @@ public class SStage extends ClientJMS{
     private MessageProducer mp1;
     private MessageProducer mp2;
     
-    private final HashMap<Integer, Formulaire> formEnAttente;
+    private final HashMap<Integer, FormulaireEnValidation> formEnAttente;
 
     public SStage() {
         this.formEnAttente = new HashMap();
@@ -64,14 +69,79 @@ public class SStage extends ClientJMS{
 
     private boolean formulaireConfirmee(int key) {
         
-        formEnAttente..;
-        for (Formulaire f : cmdsEnAttente.get(key)) {
-            if (f.getQuantite() > lc.getStock()) {
-                System.out.println("\t stock NOK sur " + c.getNumCommande());
-                return false;
-            }
+        if (formEnAttente.containsKey(key)) {
+            if (formEnAttente.get(key).getVerifEnseignement() == EtatFormulaire.VALIDEE &&
+                    formEnAttente.get(key).getVerifJuridique()== EtatFormulaire.VALIDEE &&
+                    formEnAttente.get(key).getVerifScolarite()== EtatFormulaire.VALIDEE)
+            return true;
         }
-        return true;
+        return false;
+    }
+    private void processMessage(Message msg) {
+
+        try 
+        {
+            FormulaireEnValidation form;
+            String type;
+            if (msg instanceof ObjectMessage) 
+            {
+                ObjectMessage om = (ObjectMessage) msg;
+                Object obj = om.getObject();
+                if (obj instanceof FormulaireEnValidation) 
+                {
+                    form = (FormulaireEnValidation) obj;
+                    type = om.getJMSType();
+                } else 
+                {
+                    return;
+                }
+            } else 
+            {
+                return;
+            }
+
+            if (formEnAttente.containsKey(form.getIdConv())) 
+            {
+                // Formaulaire deja recue
+                System.out.println("--> Formulaire " + form.getIdConv() + " recue");
+
+                if (formulaireConfirmee(form.getIdConv())) 
+                {
+                    
+                    System.out.println("--> Formulaire de pré-convention " + form.getIdConv() + " validée");
+                    
+                    if (type.equals(Nommage.MSG_VALIDATION_JUR))
+                            formEnAttente.get(form.getIdConv()).setVerifJuridique(form.getVerifJuridique());
+                    if (type.equals(Nommage.MSG_VALIDATION_ENS))
+                            formEnAttente.get(form.getIdConv()).setVerifJuridique(form.getVerifEnseignement());
+                    if (type.equals(Nommage.MSG_VALIDATION_SCO))
+                            formEnAttente.get(form.getIdConv()).setVerifJuridique(form.getVerifScolarite());
+                    
+                    //si 3 vérifications sont valides, on envoie un msg au département d'enseignement
+                    if(formulaireConfirmee(form.getIdConv())) 
+                    {
+                        ValidOk confirmation = new ValidOk(form.getIdConv(), form.getNumEtu(), Boolean.TRUE);
+                        ObjectMessage om = session.createObjectMessage(confirmation);
+                        om.setJMSType(Nommage.MSG_FORM_VALIDE);
+                        mp2.send(om);
+                    } else 
+                    {
+                        // On vérifie les validations manquantes
+                        System.out.println("Validation Service Juridique : " + formEnAttente.get(form.getIdConv()).getVerifJuridique());
+                        System.out.println("Validation Service Scolarité : " + formEnAttente.get(form.getIdConv()).getVerifScolarite());
+                        System.out.println("Validation Départemnt d'enseignement : " + formEnAttente.get(form.getIdConv()).getVerifEnseignement());
+                    }
+                } else 
+                {
+                    // nouvelle commande, on l'ajoute dans le dict
+                    cmdsEnAttente.put(cmd.getNumCommande(), cmd);
+                    System.out.println("--> Commande " + cmd.getNumCommande() + " mise en attente.");
+                }
+            }
+        } catch (JMSException ex) 
+        {
+            Logger.getLogger(SStage.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
         
     public static void main(String[] args) throws Exception {
